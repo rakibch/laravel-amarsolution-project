d8da66017c91ff324cb2a743e82d58c6414096b2
rakib <rakib9204@gmail.com>
2025-08-02 14:46:12 +0600

    integrated functions for admin auth controller

--- app/Http/Controllers/API/AdminAuthController.php
+++ app/Http/Controllers/API/AdminAuthController.php
@@ -4,8 +4,40 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AdminAuthController extends Controller
 {
-    //
+     public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $student = User::where('email', $request->email)->first();
+
+        if (!$student || !Hash::check($request->password, $student->password)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        $token = $student->createToken('student-token')->plainTextToken;
+
+        return response()->json([
+            'user' => $student,
+            'token' => $token
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json(['message' => 'Logged out']);
+    }
+
+    public function me(Request $request)
+    {
+        return response()->json($request->user());
+    }
 }
