730e5dceaba05e7728e11e2ab31994dd3fbd808a
rakib <rakib9204@gmail.com>
2025-08-02 14:30:23 +0600

    add 20 admin data as seed to user table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Database\Seeders\StudentSeeder;
+use Database\Seeders\AdminSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,6 +17,10 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
          StudentSeeder::class,
+        ]);
+
+        $this->call([
+         AdminSeeder::class,
         ]);
     }
 }
